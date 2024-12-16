8bdce4c3d3d69c04d8ecaaea99cd5ccbba56b006
ilann11 <ilann.stefanovitch@etu.univ-amu.fr>
2024-12-16 08:56:37 +0100

    init

---
+++ Pages/theme.js
@@ -0,0 +1,24 @@
+// Vérifie si un thème est déjà stocké dans le localStorage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    document.querySelector('header').classList.add(currentTheme);
+    document.querySelector('footer').classList.add(currentTheme);
+}
+
+// Sélectionne le bouton de basculement de thème
+const toggleButton = document.getElementById('toggle-theme');
+
+toggleButton.addEventListener('click', () => {
+    // Basculer entre les classes 'dark-mode'
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+
+    // Enregistrer ou supprimer le thème choisi dans le localStorage
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.removeItem('theme');
+    }
+});
